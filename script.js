a0ff6faa8739c6faf6c5a77198c8bd16ab0d2849
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2025-01-09 20:51:11 +0530

    Handle Outgoing Message

---
+++ script.js
@@ -0,0 +1,39 @@
+const chatBody = document.querySelector(".chat-body");
+const messageInput = document.querySelector(".message-input");
+const sendMessageButton = document.querySelector("#send-message");
+
+const userData = {
+    message: null
+}
+
+// message element with dynamic classes
+const createMessageElement = (content, classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
+
+//handle outgoing user messages
+const handleOutgoingMessage = (e) => {
+    e.preventDefault();
+    userData.message = messageInput.value.trim();
+    messageInput.value = "";
+
+    //create display user message
+    const messageContent = `<div class="message-text"></div>`;
+
+    const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
+    outgoingMessageDiv.querySelector(".message-text").textContent = userData.message;
+    chatBody.appendChild(outgoingMessageDiv);
+}
+
+// enter key for send message
+messageInput.addEventListener("keydown", (e) => {
+    const userMessage = e.target.value.trim();
+    if(e.key === "Enter" && userMessage) {
+        handleOutgoingMessage(e);
+    }
+});
+
+sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e))
